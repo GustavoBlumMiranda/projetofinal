1ad23896b95e6e4063dd726b83e77799566d5deb
Gustavo Miranda <1190201047@veigadealmeida.edu.br>
2024-09-11 18:49:58 -0300

    commit inicial

---
+++ src/test/java/com/veigadealmeida/projetofinal/ProjetofinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veigadealmeida.projetofinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetofinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
